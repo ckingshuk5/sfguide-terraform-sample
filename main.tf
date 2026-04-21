7a60d20f9a79bb5a4ca0d854527a7a3721a7f3e6
ckingshuk5 <ckingshuk5@gmail.com>
2026-04-22 01:49:04 +0530

    Create service User,Role , Schema, Grants

--- main.tf
+++ main.tf
@@ -20,3 +20,24 @@ provider "snowflake" {
     authenticator     = "SNOWFLAKE_JWT"
     private_key       = file(local.private_key_path)
 }
+resource "snowflake_database" "tf_db" {
+  name         = "FINTECT_DB"
+  is_transient = false
+}
+
+resource "snowflake_warehouse" "tf_warehouse" {
+  name                      = "COMPUTE_WH"
+  warehouse_type            = "STANDARD"
+  warehouse_size            = "XSMALL"
+  max_cluster_count         = 1
+  min_cluster_count         = 1
+  auto_suspend              = 60
+  auto_resume               = true
+  enable_query_acceleration = false
+  initially_suspended       = true
+}
+
+resource "snowflake_database" "tf_db" {
+  name         = "DP_MODEL_DB"
+  is_transient = false
+}
